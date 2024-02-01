adedc31eaa82d4d3583d6d4f2fabc0108e0fa8bc
abdulrahman albasti <220036263@aston.ac.uk>
2024-02-01 19:07:01 +0000

    remove the slash

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -4,7 +4,7 @@ void testMethod1()
   {
   //First command x = y + z
   //Second commnad print(x)
-  //// Edit made by Abdulrahman_220036263.
+  //Edit made by Abdulrahman_220036263.
 
 
   //Last line print('TA TA')
